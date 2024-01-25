51242cac46c50a8ad72b7f67ace448a16929160d
Abdul Khaleq <abdulkhaleq6008@gmail.com>
2024-01-25 16:55:40 +0600

    change in settings

--- book_donate_platform/settings.py
+++ book_donate_platform/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'star_ratings',
     "crispy_forms",
     "crispy_bootstrap5",
     'user',
